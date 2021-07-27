57e699b76e1d1ae46d230961abaa3253a13a235a
yfzz <tom.yfzz@foxmail.com>
2021-07-27 21:03:05 +0800

    完成登录和注册功能开发

---
+++ assets/js/baseAPI.js
@@ -0,0 +1,7 @@
+//每次调用$.ajax()$.get()$.post()会先效用这个函数进行拼接
+//这个函数可以拿到给AJAX的提供的配置对象
+$.ajaxPrefilter(function(options){
+    console.log(options.url );
+    //发送真正的ajax亲统一拼接路径
+    options.url='http://api-breakingnews-web.itheima.net' +options.url
+})
